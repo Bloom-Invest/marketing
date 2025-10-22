bafd31507ed2674e0741055f836efaeb83ab65c4
Eric Xiao <eric@conductor.build>
2025-10-22 18:19:57 -0400

    add marketing videos and images

---
+++ assets/collectionImages/dark/index.ts
@@ -0,0 +1,25 @@
+export { default as ackman } from './ackman.svg';
+export { default as buffett } from './buffett.svg';
+export { default as catch } from './catch.svg';
+export { default as faith } from './faith.svg';
+export { default as faucet } from './faucet.svg';
+export { default as growth } from './growth.svg'; // Marked for deletion
+export { default as rocket } from './rocket.svg';
+export { default as hype } from './hype.svg'; // Marked for removal
+export { default as megaphone } from './megaphone.svg';
+export { default as leaderboard } from './leaderboard.svg';
+export { default as piggyBank } from './piggyBank.svg';
+export { default as qualityDiscount } from './qualityDiscount.svg';
+export { default as tech } from './tech.svg';
+export { default as undervalued } from './undervalued.svg';
+export { default as week52 } from './week52.svg';
+export { default as wood } from './wood.svg';
+export { default as bell } from './bell.svg';
+export { default as newbie } from './newbie.svg';
+export { default as movers } from './movers.svg';
+export { default as shorts } from './shorts.svg';
+export { default as nancy } from './nancy.svg';
+export { default as popular } from './popular.svg';
+export { default as druck } from './druck.svg';
+export { default as bloom } from './bloom.svg';
+export { default as collections } from './collections.svg';
---
+++ assets/subscriptionImages/dark/index.ts
@@ -0,0 +1,6 @@
+export { default as confetti } from './confetti.svg';
+export { default as newbie } from './newbie.svg';
+export { default as reports } from './reports.svg';
+export { default as wiim } from './wiim.svg';
+export { default as collections } from './collections.svg';
+export { default as discord } from './discord.svg';
